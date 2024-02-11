91f37ac1d9467e5a8f5b0fc174043995ca3ff847
arthusagain <arthus.james@hotmail.com>
2024-02-11 12:27:58 -0300

    Rename lowercase

--- wwwroot/js/site.js
+++ wwwroot/js/site.js
@@ -3,7 +3,7 @@ const docFooter = document.querySelector("footer");
 docHeader.innerHTML = `
         <nav class="navbar navbar-expand-sm navbar-toggleable-sm navbar-light mb-0 bg-primary box-shadow navbar__main">
             <div class="container-fluid">
-                <a class="navbar-brand" asp-area="" href="Index.html">Árthus James Erthal</a>
+                <a class="navbar-brand" href="index.html">Árthus James Erthal</a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target=".navbar-collapse" aria-controls="navbarSupportedContent"
                         aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
@@ -11,15 +11,15 @@ docHeader.innerHTML = `
                 <div class="navbar-collapse collapse d-sm-inline-flex justify-content-between">
                     <ul class="navbar-nav flex-grow-1">
                         <li class="nav-item">
-                                <a class="nav-link" href="Projetos.html" asp-action="Projetos">Projetos</a>
+                                <a class="nav-link" href="projetos.html">Projetos</a>
                             
                         </li>
                         <li class="nav-item">
-                                <a class="nav-link" href="Curriculos.html" asp-action="Curriculos">Curriculos</a>
+                                <a class="nav-link" href="curriculos.html">Curriculos</a>
                             
                         </li>
                         <li class="nav-item">
-                                <a class="nav-link" href="Sobre.html" asp-action="Sobre">Sobre</a>
+                                <a class="nav-link" href="sobre.html">Sobre</a>
                             
                         </li>
                     </ul>
